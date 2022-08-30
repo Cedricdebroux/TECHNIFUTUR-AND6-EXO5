8b3baed0fbc8ccf8d20ed8bccd758d952b286099
Cedric Debroux <cedric@debroux.ovh>
2022-08-30 15:48:25 +0200

    fin de l'exercice final y compris le bonus

--- app/src/main/java/com/technipixl/exo5/MainActivity.kt
+++ app/src/main/java/com/technipixl/exo5/MainActivity.kt
@@ -2,48 +2,125 @@ package com.technipixl.exo5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
-import android.widget.Toolbar
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.technipixl.exo5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        drawerLayout = binding.drawerLayout
+
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        setSupportActionBar(findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar))
+        setSupportActionBar(binding.toolbar)
         val appBarConfiguration: AppBarConfiguration?
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.Home,
-                R.id.News
-            )
+                R.id.tennisFragment,
+                R.id.basketFragment,
+                R.id.handFragment
+            ), drawerLayout
         )
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             when (destination.id) {
-                R.id.Home -> findViewById<TextView>(R.id.toolbarTitle).text = resources.getString(R.string.home)
-                R.id.News -> findViewById<TextView>(R.id.toolbarTitle).text = resources.getString(R.string.news)
+                R.id.tennisFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.tennis)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.VISIBLE
+                }
+                R.id.basketFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.basket_bottom)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.VISIBLE
+                }
+                R.id.infoFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.info)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.GONE
+                }
+                R.id.pratiqueFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.pratique_title)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.GONE
+                }
+                R.id.entrainementFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.entrainement_title)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.GONE
+                }
+                R.id.planningFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.planning_title)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.GONE
+                }
+                R.id.contactFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.contact_title)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.GONE
+                }
+                R.id.handFragment -> {
+                    findViewById<TextView>(R.id.toolbarTitle).text =
+                        resources.getString(R.string.hand_bottom)
+                    findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility =
+                        View.VISIBLE
+                }
+
+
                 else -> findViewById<TextView>(R.id.toolbarTitle).text = null
             }
         }
     }
 
-    
-//    permet l'affichage de la fleche de retours
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    //    permet l'affichage de la fleche de retours
     override fun onSupportNavigateUp(): Boolean {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         return navController.navigateUp()
     }
+
+
 }
--- app/src/main/java/com/technipixl/exo5/PlanningFragment.kt
+++ app/src/main/java/com/technipixl/exo5/PlanningFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [NewsFragment.newInstance] factory method to
+ * Use the [PlanningFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NewsFragment : Fragment() {
+class PlanningFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,16 +34,7 @@ class NewsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.detailButton).setOnClickListener {
-            val action = R.id.action_News_to_detailsFragment
-            findNavController().navigate(action)
-        }
+        return inflater.inflate(R.layout.fragment_planning, container, false)
     }
 
     companion object {
@@ -55,12 +44,12 @@ class NewsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsFragment.
+         * @return A new instance of fragment PlanningFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
+            PlanningFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
